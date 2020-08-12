a1771e57928edd16675cd948ae3128f92b524ee8
ejiro-edwin <ejiroedwin@gmail.com>
2020-08-12 11:42:14 +0100

    reviews controller, options on rendering data

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -3,12 +3,12 @@ module Api
     class AirlinesController < ApplicationController
       def index
         airlines = Airline.all
-        render json: AirlineSerializer.new(airlines).serialized_json
+        render json: AirlineSerializer.new(airlines, options).serialized_json
       end
 
       def show
         airline = Airline.find_by(slug: params[:slug])
-        render json: AirlineSerializer.new(airline).serialized_json
+        render json: AirlineSerializer.new(airline, options).serialized_json
       end
 
       def create
@@ -23,7 +23,7 @@ module Api
       def update
         airline = Airline.find_by(slug: params[:slug])
         if airline.update(airline_params)
-          render json: AirlineSerializer.new(airline).serialized_json
+          render json: AirlineSerializer.new(airline, options).serialized_json
         else
           render json: {error:airline.errors.messages}, status:422
         end
@@ -39,6 +39,9 @@ module Api
 
       def airline_params
         params.require(:airline).permit(:name, :image_url)
+      end
+      def options
+        @options ||= {include: %[reviews]}
       end
     end
   end
